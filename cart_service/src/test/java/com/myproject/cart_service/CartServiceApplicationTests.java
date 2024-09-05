f51e337da372ba2dd37d1754610e9ba7a5e73811
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-09-05 19:33:00 +0700

    :))

---
+++ cart_service/src/test/java/com/myproject/cart_service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
