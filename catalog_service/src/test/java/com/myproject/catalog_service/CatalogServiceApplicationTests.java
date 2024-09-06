ca742f39656f5f3b7900d364555725152e4796e9
nguyenle <le.nn@falcongames.com>
2024-09-06 11:33:16 +0700

    feat(catalog-service): init

---
+++ catalog_service/src/test/java/com/myproject/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
