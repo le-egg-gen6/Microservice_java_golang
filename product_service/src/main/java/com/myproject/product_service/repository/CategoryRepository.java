b597fe04ba7919baa346294321e29ae1ea15599d
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-09-19 23:06:39 +0700

    fix(product-service): add paging&sorting repository

--- product_service/src/main/java/com/myproject/product_service/repository/CategoryRepository.java
+++ product_service/src/main/java/com/myproject/product_service/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.myproject.product_service.repository;
 
 import com.myproject.product_service.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +11,10 @@ import org.springframework.stereotype.Repository;
  * @since 2:30 AM Fri 9/13/2024
  */
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends
+        JpaRepository<Category, Long>,
+        PagingAndSortingRepository<Category, Long>,
+        ListPagingAndSortingRepository<Category, Long>
+{
+
 }
--- product_service/src/main/java/com/myproject/product_service/repository/PromotionRepository.java
+++ product_service/src/main/java/com/myproject/product_service/repository/PromotionRepository.java
@@ -2,11 +2,18 @@ package com.myproject.product_service.repository;
 
 import com.myproject.product_service.entity.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author nguyenle
  */
 @Repository
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+public interface PromotionRepository extends
+        JpaRepository<Promotion, Long>,
+        PagingAndSortingRepository<Promotion, Long>,
+        ListPagingAndSortingRepository<Promotion, Long>
+{
+
 }
