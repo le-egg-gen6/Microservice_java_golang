9f317da9666d9aea4bcf9bc2aba4b54c2fca6cf9
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-09-21 10:56:15 +0700

    fix(notification-service): fix socket event dynamic register

--- notification_service/src/main/java/com/myproject/notification_service/socket/config/SocketEventRegister.java
+++ notification_service/src/main/java/com/myproject/notification_service/socket/config/SocketEventRegister.java
@@ -6,6 +6,9 @@ import com.myproject.notification_service.socket.annotation.SocketEvent;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -13,6 +16,7 @@ import org.springframework.core.annotation.Order;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Set;
 
 /**
  * @author nguyenle
@@ -30,22 +34,26 @@ public class SocketEventRegister {
 
     @PostConstruct
     public void registerEvents() {
-        for (String beanName : applicationContext.getBeanDefinitionNames()) {
-            Object bean = applicationContext.getBean(beanName);
-            Class<?> beanClass = bean.getClass();
-            if (beanClass.isAnnotationPresent(SocketEvent.class)) {
-                SocketEvent annotation = beanClass.getAnnotation(SocketEvent.class);
+        Reflections reflections = new Reflections("com.myproject", new SubTypesScanner(), new TypeAnnotationsScanner());
+
+        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(SocketEvent.class);
+        for (Class<?> clazz : annotatedClasses) {
+            if (DataListener.class.isAssignableFrom(clazz)) {
+                SocketEvent annotation = clazz.getAnnotation(SocketEvent.class);
                 String eventName = annotation.eventName();
 
-                if (bean instanceof DataListener) {
-                    Type[] interfaces = beanClass.getGenericInterfaces();
-                    for (Type type : interfaces) {
-                        if (type instanceof ParameterizedType parameterizedType) {
-                            if (parameterizedType.getRawType().equals(DataListener.class)) {
-                                Class<?> dataType = (Class<?>) parameterizedType.getActualTypeArguments()[0];
-                                registerEventListener(eventName, dataType, (DataListener<?>) bean);
-                                break;
+                Type[] interfaces = clazz.getGenericInterfaces();
+                for (Type type : interfaces) {
+                    if (type instanceof ParameterizedType parameterizedType) {
+                        if (parameterizedType.getRawType().equals(DataListener.class)) {
+                            Class<?> dataType = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+                            try {
+                                Object listener = clazz.getDeclaredConstructor().newInstance();
+                                registerEventListener(eventName, dataType, listener);
+                            } catch (Exception e) {
+                                log.error("Error creating instance of " + clazz.getName(), e);
                             }
+                            break;
                         }
                     }
                 }
@@ -53,14 +61,23 @@ public class SocketEventRegister {
         }
     }
 
-    private void registerEventListener(String eventName, Class<?> dataType, DataListener<?> listener) {
+    private <T> void registerEventListener(String eventName, Class<T> dataType, Object listenerObj) {
+        if (!(listenerObj instanceof DataListener)) {
+            log.error("Listener object is not an instance of DataListener: " + listenerObj.getClass().getName());
+            return;
+        }
+
+        @SuppressWarnings("unchecked")
+        DataListener<T> typedListener = (DataListener<T>) listenerObj;
+
         socketIOServer.addEventListener(eventName, dataType, (client, data, ackRequest) -> {
             try {
-                listener.onData(client, data, ackRequest);
+                typedListener.onData(client, data, ackRequest);
             } catch (Exception e) {
-                log.error(e.getMessage());
+                log.error("Error in socket event listener: " + eventName, e);
             }
         });
+        log.info("Registered socket event listener for: " + eventName);
     }
 
 }
