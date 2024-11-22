2dfc3b23c43d79f1c5fc8e5a53256f9d1bf59c12
nguyenle <lenn@falcongames.com>
2024-11-22 17:40:45 +0700

    init(debezium_service): capture data change events for ELK

---
+++ debezium_service/src/main/java/com/myproject/debezium_service/DebeziumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.debezium_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumServiceApplication.class, args);
+	}
+
+}
---
+++ debezium_service/src/test/java/com/myproject/debezium_service/DebeziumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.debezium_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
